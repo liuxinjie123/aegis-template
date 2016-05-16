f6b0dbb8546926007f549dcb53e21b5cef853412
hary <94093146@qq.com>
2016-05-16 13:31:05 +0800

    updated

--- aegis-site-node-tpl/middlewares/passport.js
+++ aegis-site-node-tpl/middlewares/passport.js
@@ -9,7 +9,8 @@ var auth = function(req, res, next) {
     if( !req.session || !req.session.user ) {
         // 没有passportCookie, 用户没有登录过, 直接跳转到登录系统
         if ( !req.cookies[config.passportCookie]) {
-            res.redirect(config.passportLogin + '&gotoURL=' + (req.host + ':8081' + req.path) );
+            var gotoURL = req.referer;  // 这样应该是没有问题的, 因为POST后面都会加redirect, 地址栏的总是可以访问的!!!!
+            res.redirect(config.passportLogin + '?gotoURL=' + gotoURL);
             return;
         }
         // cookie中有passport, 但是session中没有用户信息, 则调用sso的认证接口
